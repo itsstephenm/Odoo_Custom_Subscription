9076f5691247e713f680c2d124421699ab29c8f6
Stephen <muregwistephen1@gmail.com>
2026-08-29 09:20:32 +0200

    pos warning update

--- static/src/js/pos_warning.js
+++ static/src/js/pos_warning.js
@@ -1,20 +1,20 @@
 /** @odoo-module **/
 
-import { PosGlobalState } from "@point_of_sale/app/store/pos_global_state";
 import { patch } from "@web/core/utils/patch";
-import { ConfirmPopup } from "@point_of_sale/app/utils/confirm_popup/confirm_popup";
+import { PosStore } from "@point_of_sale/app/services/pos_store";
+import { ConfirmationDialog } from "@web/core/confirmation_dialog/confirmation_dialog";
 
-patch(PosGlobalState.prototype, {
+patch(PosStore.prototype, {
     async setup() {
         await super.setup(...arguments);
-        
+
         const session = this.env.services.session;
         if (session && session.vast_sub_warning && !window.sessionStorage.getItem('vast_sub_warned_pos')) {
             window.sessionStorage.setItem('vast_sub_warned_pos', 'true');
-            
+
             // Show popup gracefully after standard initialization
             setTimeout(() => {
-                this.env.services.popup.add(ConfirmPopup, {
+                this.env.services.dialog.add(ConfirmationDialog, {
                     title: 'Subscription Renewal Notice',
                     body: `Your subscription renews in ${session.vast_sub_warning_days} days. Please ensure payment methods are up to date.`,
                 });
