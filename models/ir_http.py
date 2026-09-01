12d93d9e86db601267afe35d450547045ba27576
Stephen <muregwistephen1@gmail.com>
2026-09-01 15:30:59 +0200

    Remove User ID 2 immunity to ensure test clients are locked out

--- models/ir_http.py
+++ models/ir_http.py
@@ -1,6 +1,10 @@
 from odoo import models, api, fields
 from odoo.http import request
 from werkzeug.utils import redirect
+from odoo.exceptions import AccessError
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class IrHttp(models.AbstractModel):
     _inherit = 'ir.http'
@@ -11,30 +15,35 @@ class IrHttp(models.AbstractModel):
             path = request.httprequest.path
             accept_header = request.httprequest.headers.get('Accept', '')
             
-            # Catch HTML navigations across all backend routes (/web, /odoo, /action)
             is_html = 'text/html' in accept_header
             is_backend = path.startswith('/web') or path.startswith('/odoo') or 'action' in path
             is_exempt = '/web/login' in path or '/web/session' in path or '/my/' in path
             
-            if is_backend and not is_exempt and is_html:
-                env = request.env
-                user = env.user
+            if is_backend and not is_exempt:
+                uid = request.session.uid
                 
-                # STRICT LOCKOUT: Only System (1) and Vast-Solutions Tech Admin (2) are immune
-                if user and user.id not in [1, 2]:
+                # CHANGED: Only immune the absolute root System user (ID 1)
+                # If your Vast-Solutions admin needs immunity later, we will use security groups instead of hardcoded IDs.
+                if uid and uid != 1:
+                    user = request.env['res.users'].sudo().browse(uid)
                     partner = user.partner_id.commercial_partner_id
-                    sub = env['sale.order'].sudo().search([
+                    
+                    sub = request.env['sale.order'].sudo().search([
                         ('partner_id', 'child_of', partner.id),
-                        ('is_subscription', '=', True),
                         ('subscription_state', '=', 'past_due')
-                    ], limit=1, order='id desc')
+                    ], limit=1)
                     
                     if sub:
-                        return redirect('/my/invoices')
+                        _logger.info(f"LOCKOUT TRIGGERED for User ID {uid} - Redirecting or Raising AccessError")
+                        if is_html:
+                            return redirect('/my/invoices')
+                        else:
+                            raise AccessError("SYSTEM LOCKOUT: Your subscription is past due. Please navigate to the portal to pay your outstanding invoices.")
                         
         return super(IrHttp, cls)._dispatch(endpoint)
 
     def session_info(self):
+        # ... (keep your existing session_info method exactly as it is) ...
         result = super(IrHttp, self).session_info()
         user = self.env.user
         
@@ -42,7 +51,6 @@ class IrHttp(models.AbstractModel):
             partner = user.partner_id.commercial_partner_id
             sub = self.env['sale.order'].sudo().search([
                 ('partner_id', 'child_of', partner.id),
-                ('is_subscription', '=', True),
                 ('subscription_state', 'in', ['active', 'trial'])
             ], limit=1, order='next_invoice_date asc')
             
